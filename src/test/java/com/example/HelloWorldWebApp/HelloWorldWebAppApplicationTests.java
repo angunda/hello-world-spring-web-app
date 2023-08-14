fbeb96fee2ebae9efb257964c4159247e38763fc
Amar Gunda <AmarNagh@gmail.com>
2023-08-14 17:23:36 -0400

    Initial Version of HelloWorld Spring Web App created using Spring Initializr

---
+++ src/test/java/com/example/HelloWorldWebApp/HelloWorldWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
